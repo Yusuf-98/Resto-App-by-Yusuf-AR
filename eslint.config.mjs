918a7355521bc4de9548e7c120df7c6ab4b76c5f
Yusuf-98 <yusuf.smg@gmail.com>
2026-09-18 16:26:31 +0700

    fix: repair broken eslint flat-config using FlatCompat

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,15 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import nextVitals from 'eslint-config-next/core-web-vitals.js';
-import nextTs from 'eslint-config-next/typescript.js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
