0c50f31ccba7a9db187098932360c3643e59f7e6
Yusuf-98 <yusuf.smg@gmail.com>
2026-09-18 16:28:25 +0700

    perf: migrate Nunito to next/font/google, remove duplicate font load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
 import { Toaster } from '@/components/ui/toaster';
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: {
@@ -20,19 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='id' suppressHydrationWarning>
-      <head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='anonymous'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700;800&display=swap'
-          rel='stylesheet'
-        />
-      </head>
-      <body>
+      <body className={nunito.className}>
         <Providers>
           {children}
           <Toaster />
