04043c8af4b7793f543d5a8dd7449d5ae392ed8e
Yusuf-98 <yusuf.smg@gmail.com>
2026-09-21 05:51:32 +0700

    perf: optimize navbar avatar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { isUnoptimizedSrc } from '@/lib/image-hosts';
 import { useRouter, usePathname } from 'next/navigation';
 import { useState, useRef, useEffect } from 'react';
 import { useAuthStore } from '@/store/auth.store';
@@ -153,7 +154,7 @@ export function Navbar() {
                         width={48}
                         height={48}
                         className='h-full w-full object-cover'
-                        unoptimized
+                        unoptimized={isUnoptimizedSrc(user.avatar)}
                       />
                     ) : (
                       <Image
@@ -188,7 +189,7 @@ export function Navbar() {
                             width={48}
                             height={48}
                             className='h-full w-full object-cover'
-                            unoptimized
+                            unoptimized={isUnoptimizedSrc(user.avatar)}
                           />
                         ) : (
                           <Image
